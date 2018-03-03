f9f7fff335f3068ad92e1e50b9d2df5671a45051
farmani60 <farmani60@gmail.com>
2018-03-04 00:25:50 +0100

    Hamming Distance

--- substring.py
+++ substring.py
@@ -23,21 +23,57 @@ def allUnique(s, start, end):
     return True
 
 class BruteForce(object):
+    """This approch has O(n3) time complexity."""
     def lengthOfLongestSubstring(self, s):
         n = len(s)
         ans = 0
         for i in range(n-1):
-            for j in range(i+1, n):
+            for j in range(i+1, n+1):
                 if allUnique(s, i, j):
                     ans = max(ans, j-i)
         return ans
 
+class SligdingWindow(object):
+    def lengthOfLongestSubstring(self, s):
+        n = len(s)
+        i = 0
+        j = 0
+        ans = 0
+        set = ""
+        while i < n and j < n:
+            if s[j] not in set:
+                set += s[j]
+                j += 1
+                ans = max(ans, j-i)
+            else:
+                ind = set.find(s[i])
+                Lset = list(set)
+                Lset.pop(ind)
+                i += 1
+                set = "".join(Lset)
+        return ans
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        dic, res, start, = {}, 0, 0
+        for i, ch in enumerate(s):
+            # when char already in dictionary
+            if ch in dic:
+                # check length from start of string to index
+                res = max(res, i-start)
+                # update start of string index to the next index
+                start = max(start, dic[ch]+1)
+            # add/update char to/of dictionary
+            dic[ch] = i
+        # answer is either in the begining/middle OR some mid to the end of string
+        return max(res, len(s)-start)
 
 
 
 if __name__ == '__main__':
     s = 'abcabcefgh'
-    ans = BruteForce().lengthOfLongestSubstring(s)
+    ans = Solution().lengthOfLongestSubstring(s)
+    # ans = SligdingWindow().lengthOfLongestSubstring(s)
     print(ans)
 
 
