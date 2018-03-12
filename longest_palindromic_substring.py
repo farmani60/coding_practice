28f7d8f56de30890fa24b42d3b5a726de9b0f3ce
Reza Farmani <farmani60@gmail.com>
2018-03-12 22:58:51 +0100

    Update longest_palindromic_substring.py

--- longest_palindromic_substring.py
+++ longest_palindromic_substring.py
@@ -6,7 +6,7 @@
 # contiguous substring of a given string that is also a palindrome. For example
 # , the longest palindromic substring of "bananas" is "anana".
 
-class Solution(object):
+class Solution1(object):
     """Time complexity : O(n^3). Assume that n is the length of the input string, 
     there are a total of n(n-1)/2 such substrings (excluding the trivial solution 
     where a character itself is a palindrome). Since verifying each substring takes 
@@ -26,5 +26,29 @@ class Solution(object):
             print('Length of the string is more than 1000!')
         return longest_pal
 
-longest_pal = Solution().longestPalindrome('babad')
-print(longest_pal)
+
+
+class Solution2(object):
+    """Time complexity: O(n^2)
+    Space complexity: O(1)"""
+    def longestPalindrome(self, s):
+        start = 0
+        end = 0
+        for i in range(len(s)):
+            length1 = self._expandAroundCenter(s, i, i)
+            length2 = self._expandAroundCenter(s, i, i+1)
+            length = max(length1, length2)
+            if (length > end - start):
+                start = i - (length - 1) // 2
+                end = i + length // 2
+        return s[start:end+1]
+
+    def _expandAroundCenter(self, s, left, right):
+        while (left >= 0 and right < len(s) and s[left] == s[right]):
+            left -= 1
+            right += 1
+        return right-left-1
+
+
+lond_pal = Solution2().longestPalindrome('caba')
+print(lond_pal)
