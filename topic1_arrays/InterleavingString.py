f4a10fd961c247b16d41a5e1fa4e2d75a6acedeb
farmani60 <farmani60@gmail.com>
2020-05-24 19:41:06 +0200

    Interleaving string

--- topic1_arrays/InterleavingString.py
+++ topic1_arrays/InterleavingString.py
@@ -10,66 +10,20 @@ Example 2:
 Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
 Output: false
 
+The most basic idea is to find every string possible by all interleavings of the
+two given strings s1 and s2. In order to implement this method, we are using
+recursion. We start by taking the current character of the first string s1 and
+then appending all possible interleavings of the remaining portion of the first
+string s1 and the second string s2 and comparing each result formed with the
+required interleaved string s3. Similarly, we choose one character from the
+second string s2 and form all the interleavings with the remaining portion of s2
+and s1 to check if the required string s3 can be formed.
 """
 
-"""
-s1 = "abc"
-s2 = "bcd"
-s3 = "abcbdc"
-
-s1: abc
-i: 0
-s2: bcd
-j: 0
-res: ""
-s3: abcbdc
-
-s1: abc
-i: 1
-s2: bcd
-j: 0
-res: a
-s3: abcbdc
-
-s1: abc
-i: 2
-s2: bcd
-j: 0
-res: ab
-s3: abcbdc
-
-s1: abc
-i: 3
-s2: bcd
-j: 0
-res: abc
-s3: abcbdc
-
-s1: abc
-i: 3
-s2: bcd
-j: 1
-res: abcb
-s3: abcbdc
-
-s1: abc
-i: 3
-s2: bcd
-j: 2
-res: abcbc
-s3: abcbdc
-
-s1: abc
-i: 3
-s2: bcd
-j: 3
-res: abcbcd
-s3: abcbdc
-"""
 class Solution(object):
     """
     Time complexity: O(2**(m+n)). m is the length of s1 and n is the length of s2.
-    Spce complexity: O(m+n). The size of stack for recursive calls can go upto m+n.
+    Space complexity: O(m+n). The size of stack for recursive calls can go upto m+n.
     """
     def isInterleave(self, s1, s2, s3):
         """
@@ -85,14 +39,15 @@ class Solution(object):
             return True
         ans = False
         if i < len(s1):
-            ans |= self._isInterleave(s1, i + 1, s2, j, res + s1[i], s3)
+            ans |= self._isInterleave(s1, i + 1, s2, j, res+s1[i], s3)
         if j < len(s2):
-            ans |= self._isInterleave(s1, i, s2, j + 1, res + s2[j], s3)
+            ans |= self._isInterleave(s1, i, s2, j + 1, res+s2[j], s3)
         return ans
 
 if __name__ == "__main__":
-    s1 = "aabcc"
-    s2 = "dbbca"
-    s3 = "aadbbbaccc"
+    # s1 = "aabcc"
+    # s2 = "dbbca"
+    # s3 = "aadbbcbcac"
+    s1, s2, s3 = "aa", "b", "aba"
     s = Solution()
     print(s.isInterleave(s1, s2, s3))
