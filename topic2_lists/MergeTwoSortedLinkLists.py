fa3502ea3d777216f6cf21161e97f038ba5c5ff1
Reza Farmani <farmani60@gmail.com>
2020-05-26 17:26:32 +0200

    Update MergeTwoSortedLinkLists.py
    update merge two sorted linked lists

--- topic2_lists/MergeTwoSortedLinkLists.py
+++ topic2_lists/MergeTwoSortedLinkLists.py
@@ -1,17 +1,31 @@
 class Node:
-    def __init__(self, data):
+    def __index__(self, data):
         self.data = data
-        self.nextnode = None
+        self.nextdone = None
 
 def mergeLists(head1, head2):
-    pass
+    x = Node(min(head1.data, head2.data))
+    node1 = head1
+    node2 = head2
+    while (node1.data is not None) or (node1.nextnode is not None):
+        while (node2.data is not None) or (node2.nextnode is not None):
+            if node1.data <= node2.data:
 
-def appendNodeInNewList(head, tail, data):
+def AddInBetween(headnode, data, position):
     NewNode = Node(data)
-    if head is not None:
-        tail.nextnode = NewNode
-    return NewNode
-
+    if headnode is None:
+        return NewNode
+    if position == 0:
+        NewNode.nextdone = headnode
+        return NewNode
+    node = headnode
+    currPosition = 0
+    while (currPosition < position - 1) and (node.nextdone is not None):
+        node = node.nextdone
+        currPosition += 1
+    NewNode.nextdone = node.nextdone
+    node.nextdone = NewNode
+    return headnode
 
 
 e1 = Node(1)
@@ -25,5 +39,3 @@ f1 = Node(3)
 f2 = Node(4)
 
 f1.nextnode = f2
-
-mergeLists(e1, f1)
