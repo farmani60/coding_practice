195a8b00153ac78a8e16e89387106c39108349e9
fariba faezi <fariba86@faribas-MacBook.local>
2020-05-27 22:23:56 +0200

    Balanced Brackets

---
+++ topic3_stacks_queues/BalancedBrackets.py
@@ -0,0 +1,14 @@
+def isBalanced(expression):
+    stack = []
+    for c in expression:
+        if c == "{":
+            stack.append("}")
+        elif c == "[":
+            stack.append("]")
+        elif c == "(":
+            stack.append(")")
+        else:
+            if (not stack) or (c != stack[-1]):
+                return False
+            stack.pop()
+    return not stack
