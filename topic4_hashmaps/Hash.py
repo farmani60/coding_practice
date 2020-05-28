96baf26971d21251f740d1824ca6c3c95073bd8a
fariba faezi <farmani60@gmail.com>
2020-05-28 23:52:45 +0200

    hash tables

--- topic4_hashmaps/Hash.py
+++ topic4_hashmaps/Hash.py
@@ -28,3 +28,25 @@ print(dict)
 del dict
 
 # To study: https://dbader.org/blog/python-dictionaries-maps-and-hashtables
+squares = {x: x**2 for x in range(10)}
+print(squares)
+print(squares[0].__hash__)
+
+import collections
+
+d = collections.OrderedDict(one=1, two=2, three=3)
+print('\n')
+d['four'] = 4
+print('\n')
+print(d)
+print('\n')
+print(d.keys())
+
+from collections import defaultdict
+dd = defaultdict(list)
+dd['dogs'].append('Rufus')
+dd['dogs'].append('Kathrin')
+dd['dogs'].append('Mr Sniffles')
+
+print(dd)
+
