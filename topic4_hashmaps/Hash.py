ae9300e82bde7e52cf29837c12d03fbe5b96711f
fariba faezi <farmani60@gmail.com>
2020-05-28 23:35:36 +0200

    hash tables

---
+++ topic4_hashmaps/Hash.py
@@ -0,0 +1,28 @@
+
+# Accessing values in dictionary
+
+# Declare a dictionary
+dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
+
+# Accessing the dictionary with its key
+print("dict['Name']: ", dict['Name'])
+print("dict['Age']: ", dict['Age'])
+
+# Updating existing entry
+dict['Age'] = 8
+# Add new entry
+dict['School'] = 'DPS School'
+
+print("dict['School']: ", dict['School'])
+print("dict['Age']: ", dict['Age'])
+
+# Remove entry with key Name
+del dict['Name']
+print(dict)
+
+# Remove all the entries in dictionary
+dict.clear()
+print(dict)
+
+# Delete entire dictionary
+del dict
