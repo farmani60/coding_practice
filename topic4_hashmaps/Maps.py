202311923bcc1999bba8015fc1441240e71e24dc
fariba faezi <farmani60@gmail.com>
2020-05-28 23:18:55 +0200

    Maps introduction completed

--- topic4_hashmaps/Maps.py
+++ topic4_hashmaps/Maps.py
@@ -1,5 +1,6 @@
 import collections
 
+# Creating Chain Map
 dict1 = {'day1': 'Mon', 'day2': 'Tue'}
 dict2 = {'day3': 'Wed', 'day4': 'Thu'}
 
@@ -17,3 +18,20 @@ print('elements:')
 for key, val in res.items():
     print('{} = {}'.format(key, val))
 print()
+
+# Map reordering
+res1 = collections.ChainMap(dict1, dict2)
+res2 = collections.ChainMap(dict2, dict1)
+
+print(res1.maps)
+print("\n")
+print(res2.maps)
+print("\n")
+
+# Updating Map
+print(res.maps)
+print("\n")
+dict2["day4"] = "Fri"
+print(res.maps)
+print("\n")
+
