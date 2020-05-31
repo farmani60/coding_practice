625801c2ac4d335a9f8470cdb9ea191b53047ae6
farmani60 <farmani60@gmail.com>
2020-05-31 21:35:59 +0200

    Insertion sort 1

--- topic5_sorting/InsertionSortPart1.py
+++ topic5_sorting/InsertionSortPart1.py
@@ -1,13 +1,13 @@
+# Description:
+# https://www.hackerrank.com/challenges/insertionsort1/problem?h_r=internal-search
 
-def insertionSort(input_list):
-    for i in range(1, len(input_list)):
-        j = i - 1
-        next_element = input_list[i]
-        while (input_list[j] > next_element) and (j >= 0):
-            input_list[j+1] = input_list[j]
-            j -= 1
-        input_list[j+1] = next_element
+def insertionSort1(arr):
+    for i in range(len(arr)):
+        if arr[i] > arr[-1]:
+            temp = arr[i]
+            arr[i] = arr[-1]
+            arr[-1] = temp
 
-input_list = [5, 1, 3, 9, 7, 4]
-insertionSort(input_list)
+input_list = [2, 4, 6, 8, 3]
+insertionSort1(input_list)
 print(input_list)
---
+++ topic5_sorting/InsertionSortPart2.py
@@ -0,0 +1,2 @@
+# Description:
+# https://www.hackerrank.com/challenges/insertionsort2/problem?h_r=internal-search
