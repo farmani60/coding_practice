6ab6c661f0927d72a41de60f44b94e955bccecab
farmani60 <farmani60@gmail.com>
2020-06-01 22:01:46 +0200

    binary tree insertion

---
+++ topic5_sorting/CorrectnessAndTheLoopInvariant.py
@@ -0,0 +1,3 @@
+# Description:
+# https://www.hackerrank.com/challenges/correctness-invariant/problem?h_r=internal-search
+
---
+++ topic6_trees/BinaryTreeInsertion.py
@@ -0,0 +1,39 @@
+# Description:
+# https://www.hackerrank.com/challenges/binary-search-tree-insertion/problem
+
+class Tree:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def Insert(self, data):
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Tree(data)
+                else:
+                    self.left.Insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Tree(data)
+                else:
+                    self.right.Insert(data)
+        else:
+            self.data = data
+
+    def PrintTree(self):
+        if self.left:
+            self.left.PrintTree()
+        print(self.data)
+        if self.right:
+            self.right.PrintTree()
+
+tree = Tree(4)
+tree.Insert(2)
+tree.Insert(1)
+tree.Insert(3)
+tree.Insert(7)
+tree.Insert(6)
+
+tree.PrintTree()
