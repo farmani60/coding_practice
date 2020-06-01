97cc6a942e24717383d312f1223742d8c23c9dfc
fariba faezi <farmani60@gmail.com>
2020-06-02 00:18:15 +0200

    Fibbonaci

--- topic8_recursion/Fibonacci.py
+++ topic8_recursion/Fibonacci.py
@@ -1,1 +1,12 @@
+"""
+1, 1, 2, 3, 5, 8, 13, 21
+"""
 
+def Fibonacci(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return Fibonacci(n-2) + Fibonacci(n-1)
+
+print([Fibonacci(x) for x in range(1, 8)])
