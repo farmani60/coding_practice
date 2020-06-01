cb49647d9bda2d005bf68a11c1b5c981474bd4d0
farmani60 <farmani60@gmail.com>
2020-06-01 22:47:24 +0200

    Height of Binary Tree

---
+++ topic6_trees/HeightOfABinaryTree.py
@@ -0,0 +1,14 @@
+"""
+Description:
+https://www.hackerrank.com/challenges/tree-height-of-a-binary-tree/problem
+
+The height of a binary tree is the number of edges between the tree's root and
+its furthest leaf. For example, the following binary tree is of height 2:
+
+       4
+     /   \
+    2     6
+  /  \   | \
+1    3  5   7
+
+"""
