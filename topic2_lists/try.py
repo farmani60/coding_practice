df49dfdc3c0ea4b3a2a98487bc27058eb9fd63d0
farmani60 <farmani60@gmail.com>
2020-06-06 20:55:54 +0200

    practice

--- topic2_lists/try.py
+++ topic2_lists/try.py
@@ -6,23 +6,15 @@ class Node:
 
 class LinkedList:
     def __init__(self):
-        self.head = Node
+        self.head = None
 
-    def printList(self):
-        if self.head is None:
-            return
-        printval = self.head
-        while printval:
-            print(printval.data, end=" ")
-            printval = printval.next
-
-    def inBeginning(self, newdata):
+    def atBeginnign(self, newdata):
         NewNode = Node(newdata)
         if self.head is None:
             self.head = NewNode
-        else:
-            NewNode.next = self.head
-            self.head = NewNode
+            return
+        NewNode.next = self.head
+        self.head = NewNode
 
     def atEnd(self, newdata):
         NewNode = Node(newdata)
@@ -34,40 +26,27 @@ class LinkedList:
             laste = laste.next
         laste.next = NewNode
 
-    def inBetween(self, newdata, middle_key):
+    def inBetween(self, middle_node, newdata):
         NewNode = Node(newdata)
-        NewNode.next = middle_key.next
-        middle_key.next = NewNode
+        if middle_node is None:
+            return
+        NewNode.next = middle_node.next
+        middle_node.next = NewNode
 
     def removeNode(self, key):
-        head = self.head
-        if head is not None:
-            if head == key:
-                self.head = head.next
-                head = None
+        if self.head is not None:
+            if self.head.data == key:
+                self.head = self.head.next
                 return
+        head = self.head
         while head:
-            prev = head
-            if head.next == key:
-                head = head.next
+            if head.data == key:
                 break
+            prev = head
             head = head.next
         if head is None:
             return
         prev.next = head.next
-
-
-def has_cycle(head):
-    if head is None:
-        return False
-    node = head
-    node_list = []
-    while node:
-        if node in node_list:
-            return True
-        else:
-            node_list.append(node)
-            node = node.next
-    return False
+        head = None
 
 
