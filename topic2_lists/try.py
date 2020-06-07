d18056b428770e856a12c2ccb6f76e1a929d1e01
farmani60 <farmani60@gmail.com>
2020-06-07 17:51:04 +0200

    waiter update

--- topic2_lists/try.py
+++ topic2_lists/try.py
@@ -1,43 +1,57 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
 
-def mergeLists(head1, head2):
-    if (head1 is None) and (head2 is not None):
-        return head2
-    if (head1 is not None) and (head2 is None):
-        return head1
-    if (head1 is None) and (head2 is None):
-        return  None
-    if head1.data <= head2.data:
-        head1.next = mergeLists(head1.next, head2)
-    else:
-        temp = head2
-        head2 = head2.next
-        temp.next = head1
-        head1 = temp
-        head1.next = mergeLists(head1.next, head2)
-    return head1
 
-def printList(head):
-    if head is None:
-        return
-    printval = head
-    while printval:
-        print(printval.data, end=" ")
-        printval = printval.next
+def isBalanced(expression):
+    stack = list()
+    for c in expression:
+        if c == '{':
+            stack.append('}')
+        elif c == '[':
+            stack.append(']')
+        elif c == '(':
+            stack.append(')')
+        else:
+            if (not stack) or (stack[-1] != c):
+                return False
+            stack.pop()
+    return not stack
 
-e1 = Node(1)
-e2 = Node(2)
-e3 = Node(3)
-e2.next = e3
-e1.next = e2
 
-f1 = Node(3)
-f2 = Node(4)
-f1.next = f2
+class Stack:
+    def __init__(self):
+        self.stack = []
+    def add(self, data):
+        if data not in self.stack:
+            self.stack.append(data)
+            return True
+        else:
+            return False
+    def peek(self):
+        return self.stack[-1]
+    def remove(self):
+        if len(self.stack) <= 0:
+            return False
+        else:
+            return self.stack.pop()
 
-l = mergeLists(e1, f1)
-printList(l)
+class Queue:
+    def __init__(self):
+        self.queue = []
+    def add(self, data):
+        if data not in self.queue:
+            self.queue.insert(0, data)
+            return True
+        return False
+    def size(self):
+        return len(self.queue)
+    def remove(self):
+        if len(self.queue) > 0:
+            return self.queue.pop()
 
+AStack = Stack()
+AStack.add("Mon")
+AStack.add("Tue")
+AStack.add("Wed")
+AStack.add("Thu")
+print(AStack.remove())
+print(AStack.remove())
+print(AStack.peek())
