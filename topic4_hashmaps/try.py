c8bb323d1373f1c6fce5eca3d81d8d9114d0a145
farmani60 <farmani60@gmail.com>
2020-06-07 18:07:24 +0200

    update

--- topic4_hashmaps/try.py
+++ topic4_hashmaps/try.py
@@ -1,25 +1,22 @@
-"""
-Description:
-https://www.hackerrank.com/challenges/waiter/problem?h_r=internal-search
-"""
-# create first 1000 prime numbers
+
 lower = 2
 upper = 1000
+
 prime_numbers = [i for i in range(lower, upper+1)
-                 if all([i % j == 0 for j in range(2, i)])]
+                 if all([i%j != 0 for j in range(2, i)])]
 
 class Stack:
     def __init__(self):
-        self.stack = list()
+        self.stack = []
     def add(self, data):
         self.stack.append(data)
     def remove(self):
         self.stack.pop()
 
-def Waiter(A0, iterNum):
+def Waiter(A0, Q):
     A = [A0]
     B = [None]
-    for i in range(iterNum):
+    for i in range(Q):
         a = Stack()
         b = Stack()
         for x in A[i][::-1]:
@@ -30,12 +27,3 @@ def Waiter(A0, iterNum):
         A.append(a.stack)
         B.append(b.stack)
     del B[0]
-    return A, B
-
-
-# A0 = [3, 4, 7, 6, 5]
-A0 = [3, 3, 4, 4, 9]
-A, B = Waiter(A0, 2)
-print(A)
-print(B)
-
